93f5ce55b0f164baa72534995e70f62f7a6c3e43
jonathanGarciaUgalde <jonathan-garcia@Jonathan>
2016-02-19 23:05:57 -0600

    ultima prueba para comprobar la funcion  del proyecto

---
+++ CrazyBreakout/src/cr/ac/itcr/logica/analizar.cpp
@@ -0,0 +1,18 @@
+/*
+ * analizar.cpp
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#include "analizar.h"
+
+analizar::analizar() {
+	// TODO Auto-generated constructor stub
+
+}
+
+analizar::~analizar() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CrazyBreakout/src/cr/ac/itcr/logica/analizar.h
@@ -0,0 +1,17 @@
+/*
+ * analizar.h
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#ifndef ANALIZAR_H_
+#define ANALIZAR_H_
+
+class analizar {
+public:
+	analizar();
+	virtual ~analizar();
+};
+
+#endif /* ANALIZAR_H_ */
