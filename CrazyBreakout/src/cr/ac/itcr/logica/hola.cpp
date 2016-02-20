0a32bbe4a7751ec1d810994b9ddb17662da30af4
jonathanGarciaUgalde <jonathan-garcia@Jonathan>
2016-02-19 18:29:12 -0600

    segundo

---
+++ CrazyBreakout/src/cr/ac/itcr/logica/hola.cpp
@@ -0,0 +1,18 @@
+/*
+ * hola.cpp
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#include "hola.h"
+
+hola::hola() {
+	// TODO Auto-generated constructor stub
+
+}
+
+hola::~hola() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CrazyBreakout/src/cr/ac/itcr/logica/hola.h
@@ -0,0 +1,17 @@
+/*
+ * hola.h
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#ifndef CR_AC_ITCR_LOGICA_HOLA_H_
+#define CR_AC_ITCR_LOGICA_HOLA_H_
+
+class hola {
+public:
+	hola();
+	virtual ~hola();
+};
+
+#endif /* CR_AC_ITCR_LOGICA_HOLA_H_ */
