dc2d6a5ccbd8e45b237c95edcbe2f33c85bcefbe
jonathanGarciaUgalde <jonathan-garcia@Jonathan>
2016-02-19 18:21:48 -0600

    hola

---
+++ CrazyBreakout/src/cr/ac/itcr/logica/ejemplo.cpp
@@ -0,0 +1,18 @@
+/*
+ * ejemplo.cpp
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#include "ejemplo.h"
+
+ejemplo::ejemplo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ejemplo::~ejemplo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CrazyBreakout/src/cr/ac/itcr/logica/ejemplo.h
@@ -0,0 +1,17 @@
+/*
+ * ejemplo.h
+ *
+ *  Created on: 19 de feb. de 2016
+ *      Author: jonathan-garcia
+ */
+
+#ifndef CR_AC_ITCR_LOGICA_EJEMPLO_H_
+#define CR_AC_ITCR_LOGICA_EJEMPLO_H_
+
+class ejemplo {
+public:
+	ejemplo();
+	virtual ~ejemplo();
+};
+
+#endif /* CR_AC_ITCR_LOGICA_EJEMPLO_H_ */
